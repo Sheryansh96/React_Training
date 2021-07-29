27e32c62299a82b524304ec1d5d53228c0a5f0fd
shreyansh <shreyanshrocknroll@gmail.com>
2021-07-29 10:28:16 +0530

    Commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Components/Greet' 
+import Welcome from './Components/Welcome'
+import Hello from './Components/Hello'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Clark" heroName="Superman">
+      <p> This is Childern Props </p>
+      </Greet>
+      <Greet name="Bruce" heroName="Batman">
+      <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="WonderWoman"/>
+      <Welcome name="Clark" heroName="Superman"></Welcome>
+      <Welcome name="Bruce" heroName="Batman"></Welcome> 
+      <Welcome name="Diana" heroName="WonderWoman"></Welcome> 
+      {/*<Hello></Hello> */}
     </div>
   );
 }
